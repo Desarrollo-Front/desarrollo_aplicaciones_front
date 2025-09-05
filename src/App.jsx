6093c4967f134493027b061fc2b8b18538dc3bc0
calde1104 <felicalde44@gmail.com>
2025-09-05 14:11:04 -0300

    Fix de Pagos-Lsita(falta lo de prestador) y eliminacion de vista sin uso

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PagosLista from "./views/Pagos-Lista";
 import PagosDetalle from "./views/Pagos-Detalle";
-import Intenciones from "./views/Intenciones";
-import Facturas from "./views/Facturas";
-import Disputas from "./views/Disputas";
 import Login from "./views/Login";
 const Placeholder = ({ title }) => <div style={{ padding: 24 }}>{title}</div>;
 import "./App.css";
@@ -18,11 +15,6 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/pagos" element={<PagosLista />} />
           <Route path="/detalle/:id" element={<PagosDetalle />} />
-          <Route path="/intenciones" element={<Intenciones />} />
-          <Route path="/facturas" element={<Facturas />} />
-          <Route path="/disputas" element={<Disputas />} />
-          <Route path="/reconciliaciones" element={<Placeholder title="Reconciliaciones" />} />
-          <Route path="/auditoria" element={<Placeholder title="Auditoría" />} />
         </Routes>
       </main>
     </BrowserRouter>
