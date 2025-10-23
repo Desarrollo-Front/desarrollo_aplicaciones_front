afae8a63cbc40568eb1d538b1cca582f92dd367d
calde1104 <felicalde44@gmail.com>
2025-10-23 08:12:35 -0300

    espacio entre icono y titulo

--- src/views/Pagos-Detalle.jsx
+++ src/views/Pagos-Detalle.jsx
@@ -533,10 +533,10 @@ window.onload = function(){window.print();}
             </div>
             <div className="payment-detail-info-grid">
               <div className="payment-detail-info-item">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="payment-detail-info-label-icon">
+                  <path d="M9 12L11 14L15 10M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
                 <div className="payment-detail-info-label">
-                  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9 12L11 14L15 10M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                  </svg>
                   Descripción
                 </div>
                 <div className="payment-detail-info-value">{pago.descripcion || '—'}</div>
